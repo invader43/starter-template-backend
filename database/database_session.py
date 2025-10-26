a38af21993b11818588aacaaf4332b40e4ee9fe2
Kartik <kartik.sharma@formi.co.in>
2025-10-26 13:07:24 +0000

    updated template for database models

--- database/database_session.py
+++ database/database_session.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from contextlib import asynccontextmanager
-from database.models.User import User
+from models.User import User
 
 
 DATABASE_URL = "postgresql+asyncpg://user:pass@localhost:5432/db"
@@ -25,15 +25,15 @@ async def get_async_db_session() -> AsyncSession:
         finally:
             await session.close()
 
-# Example usage
-import asyncio
-from sqlalchemy.future import select
+# # Example usage
+# import asyncio
+# from sqlalchemy.future import select
 
-async def fetch_users():
-    async with get_async_db_session() as session:
-        result = await session.execute(select(User))
-        users = result.scalars().all()
-        print([user.name for user in users])
+# async def fetch_users():
+#     async with get_async_db_session() as session:
+#         result = await session.execute(select(User))
+#         users = result.scalars().all()
+#         print([user.name for user in users])
 
-if __name__ == "__main__":
-    asyncio.run(fetch_users())
+# if __name__ == "__main__":
+#     asyncio.run(fetch_users())
